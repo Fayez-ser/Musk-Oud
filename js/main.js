8c68f60a5836d760e3c0f6575c6330ddd3619677
Fayez-Serawan <serawanfayez@gmail.com>
2025-07-15 16:34:29 +0300

    Uploading Book Perfume section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+  const element = document.getElementById('choices-select');
+  const choices = new Choices(element, {
+    placeholderValue: 'اختر نوع البيانات',
+    searchPlaceholderValue: 'ابحث...',
+    removeItemButton: true
+  });
+
+
+
