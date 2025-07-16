6e59dfb89b5c6613b39262751134097f87c88101
Fayez-Serawan <serawanfayez@gmail.com>
2025-07-16 14:30:38 +0300

    End of interface development without animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
   const element = document.getElementById('choices-select');
   const choices = new Choices(element, {
-    placeholderValue: 'اختر نوع البيانات',
-    searchPlaceholderValue: 'ابحث...',
+    placeholderValue: 'Choose the data type',
+    searchPlaceholderValue: 'Search',
     removeItemButton: true
   });
+
+    const videoModal = document.getElementById('videoModal');
+    const videoIframe = document.getElementById('videoIframe');
+    const youtubeLink = 'https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1';
+
+    videoModal.addEventListener('show.bs.modal', function () {
+      videoIframe.src = youtubeLink + "?autoplay=1";
+    });
+
+    videoModal.addEventListener('hidden.bs.modal', function () {
+      videoIframe.src = " ";
+    });
+
+
 
 
 
